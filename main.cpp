a25aa08cd37af9d316380d7b1d75df23e3ed6303
muntan <serdar_tst@hotmail.com>
2022-10-21 00:47:11 +0200

    so much pain.

--- main.cpp
+++ main.cpp
@@ -16,42 +16,75 @@
 using namespace std;
 
 class Coordinate {
-    double x,y,z;
+    float x,y,z;
     int point;//1 when point, 0 when vector
 public:
-    Coordinate(double x, double y, double z, int point) : x(x), y(y), z(z), point(point) {}
+    Coordinate(float x, float y, float z, int point) : x(x), y(y), z(z), point(point) {}
 
-    void setCoords(double x, double y, double z, int point) {
+    Coordinate operator - (Coordinate b) {
+        Coordinate diff(x-b.getX(), y-b.getY(), z-b.getZ(), 0);
+        return diff;
+    }
+
+    Coordinate operator + (Coordinate b) {
+        Coordinate diff(x+b.getX(), y+b.getY(), z+b.getZ(), 0);
+        if (point or b.isPoint()) {
+            diff.setPoint(1);
+        }
+        return diff;
+    }
+
+    Coordinate operator * (float b) {
+        Coordinate diff(x*b, y*b, z*b, 0);
+        return diff;
+    }
+
+    void normalise() {
+        float length = sqrt(pow(x,2) + pow(y,2) + pow(z,2));
+        x = x/length;
+        y = y/length;
+        z = z/length;
+    }
+
+    void setCoords(float x, float y, float z, int point) {
         x = x;
         y = y;
         z = z;
         point = point;
     }
 
-    double getX() const {
+    float getX() const {
         return x;
     }
-    double getY() const {
+    float getY() const {
         return y;
     }
-    double getZ() const {
+    float getZ() const {
         return z;
     }
     int isPoint() const {
         return point;
     }
 
+    void setPoint(int point) {
+        Coordinate::point = point;
+    }
+
     void print() {
         cout << endl << "(" << x << "," << y << "," << z << "," << point <<  ")" << endl;
     }
 
-    double distance(Coordinate b) {
-        double temp = sqrt(pow(x - b.getX(),2) + pow(y - b.getY(),2) + pow(z - b.getZ(),2));
+    float distance(Coordinate b) {
+        float temp = sqrt(pow(x - b.getX(),2) + pow(y - b.getY(),2) + pow(z - b.getZ(),2));
         return temp;
     }
 
-    double dot(Coordinate b) {
-        double temp = x * b.getX() + y * b.getY() + z * b.getZ();
+    float dot(Coordinate b) {
+        float temp = x * b.getX() + y * b.getY() + z * b.getZ();
+        return temp;
+    }
+    Coordinate cross(Coordinate b) {
+        Coordinate temp(y * b.getZ() - z * b.getY(), z * b.getX() - x * b.getZ(), x * b.getY() - y * b.getX(),0);
         return temp;
     }
 
@@ -99,10 +132,10 @@ public:
 };
 
 class Sphere : public Object {
-    double radius;
+    float radius;
     float matrix[4][4];
 public:
-    Sphere(const Coordinate &center, int r, int g, int b, double radius) : Object(center, r, g, b), radius(radius) {
+    Sphere(const Coordinate &center, int r, int g, int b, float radius) : Object(center, r, g, b), radius(radius) {
         for (int i=0;i<4;i++){
             for (int j=0;j<4;j++){
                 matrix[i][j] = 0;
@@ -118,34 +151,34 @@ public:
 
 
     Coordinate hit(Ray ray) {
-        //Coordinate tempOrig(ray.getOrigin().getX()-sphere.getTranslationX(),ray.getOrigin().getY()-sphere.getTranslationY(),ray.getOrigin().getZ()-sphere.getTranslationZ(),1);
-        //Coordinate tempDir(ray.getDirection().getX()-sphere.getTranslationX(),ray.getDirection().getY()-sphere.getTranslationY(),ray.getDirection().getZ()-sphere.getTranslationZ(),0);
+        Coordinate tempOrig(ray.getOrigin().getX()- matrix[3][0],ray.getOrigin().getY()- matrix[3][1],ray.getOrigin().getZ()- matrix[3][2],1);
+        Coordinate tempDir(ray.getDirection().getX()- matrix[3][0],ray.getDirection().getY()- matrix[3][1],ray.getDirection().getZ()- matrix[3][2],0);
 
-        //Ray genRay(tempOrig, tempDir);
+        Ray genRay(tempOrig, tempDir);
 
-        double A = pow(ray.getDirection().getX(),2) + pow(ray.getDirection().getY(),2) + pow(ray.getDirection().getZ(),2);
-        double B = (2*ray.getOrigin().getX() * ray.getDirection().getX()) + (2*ray.getOrigin().getY()*ray.getDirection().getY()) + (2*ray.getOrigin().getZ() * ray.getDirection().getZ());
-        double C = pow(ray.getOrigin().getX(),2) + pow(ray.getOrigin().getY(),2) + pow(ray.getOrigin().getZ(),2) - pow(radius,2);
+        float A = pow(genRay.getDirection().getX(),2) + pow(genRay.getDirection().getY(),2) + pow(genRay.getDirection().getZ(),2);
+        float B = (2*genRay.getOrigin().getX() * genRay.getDirection().getX()) + (2*genRay.getOrigin().getY()*genRay.getDirection().getY()) + (2*genRay.getOrigin().getZ() * genRay.getDirection().getZ());
+        float C = pow(genRay.getOrigin().getX(),2) + pow(genRay.getOrigin().getY(),2) + pow(genRay.getOrigin().getZ(),2) - pow(radius,2);
 
-        double discrim = B*B - 4 * A * C;
+        float discrim = B*B - 4 * A * C;
 
         if (discrim<0) {
             //cout << endl << "No hit." << endl;
             Coordinate temp(0,0,0,0);
-            return temp;
+            return temp; //returns a coordinate with 0 as the last value when no hit is found
         }
 
-        double t1 = (-B + sqrt(discrim))/(2*A);
-        double t2 = (-B - sqrt(discrim))/(2*A);
+        float t1 = (-B + sqrt(discrim))/(2*A);
+        float t2 = (-B - sqrt(discrim))/(2*A);
 
-        Coordinate i1(ray.getOrigin().getX() + ray.getDirection().getX() * t1,
-                      ray.getOrigin().getY() + ray.getDirection().getY() * t1,
-                      ray.getOrigin().getZ() + ray.getDirection().getZ() * t1,
+        Coordinate i1(genRay.getOrigin().getX() + genRay.getDirection().getX() * t1 + matrix[3][0],
+                      genRay.getOrigin().getY() + genRay.getDirection().getY() * t1 + matrix[3][1],
+                      genRay.getOrigin().getZ() + genRay.getDirection().getZ() * t1 + matrix[3][2],
                       1);
 
-        Coordinate i2(ray.getOrigin().getX() + ray.getDirection().getX() * t2,
-                      ray.getOrigin().getY() + ray.getDirection().getY() * t2,
-                      ray.getOrigin().getZ() + ray.getDirection().getZ() * t2,
+        Coordinate i2(genRay.getOrigin().getX() + genRay.getDirection().getX() * t2 + matrix[3][0],
+                      genRay.getOrigin().getY() + genRay.getDirection().getY() * t2 + matrix[3][1],
+                      genRay.getOrigin().getZ() + genRay.getDirection().getZ() * t2 + matrix[3][2],
                       1);
 
         cout << endl << "Hit enter:";
@@ -167,19 +200,9 @@ public:
 int main(int argc, char *argv[]) {
 
     int screenX = 1280, screenY = 720;
-    int W = screenX/2, H = screenY/2; // Technically H = N*tan(theta/2) and W = H*aspect
-    int N = 50; //distance between eye and screen
-    int u[screenX],v[screenY];
-
-    for (int c = 0; c < screenX; c++) {
-        u[c] = (-W + (W * 2 * c) / screenX);
-    }
-    for (int r = 0; r < screenY; r++) {
-        v[r] = (-H + (H * 2 * r) / screenY);
-    }
 
     Coordinate centerSphere(0, 0, 0, 1);
-    Sphere sphere(centerSphere, 148,0,211, 1);
+    Sphere sphere(centerSphere, 255,0,0, 1);
 
     Coordinate originRay(-3, 0, 0, 1);
     Coordinate directionRay(-2, 0, 0, 0);
@@ -188,13 +211,22 @@ int main(int argc, char *argv[]) {
     Coordinate i1 = sphere.hit(firstRay);
 
     Coordinate centerSphere2(3, 3, 3, 1);
-    Sphere sphere2(centerSphere, 0, 255, 0, 1);
+    Sphere sphere2(centerSphere2, 0, 255, 0, 1);
 
-    Coordinate originRay2(0, 0, 0, 1);
-    Coordinate directionRay2(1, 1, 1, 0);
-    Ray firstRay2(originRay, directionRay);
+    Coordinate originRay2(0, 3, 3, 1);
+    Coordinate directionRay2(1, 3, 3, 0);
+    Ray firstRay2(originRay2, directionRay2);
 
     sphere2.hit(firstRay2);
+
+    Coordinate centerSphere3(  0.0, 10.0, 0.0, 1);
+    Sphere sphere3(centerSphere3, 0, 255, 0, 3.0);
+
+    Coordinate originRay3(0, 3, 3, 1);
+    Coordinate directionRay3(1, 3, 3, 0);
+    Ray firstRay3(originRay3, directionRay3);
+
+    sphere3.hit(firstRay3);
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -210,11 +242,61 @@ int main(int argc, char *argv[]) {
     glClearColor(0.0,0.0,0.0,0.0);
     glMatrixMode(GL_PROJECTION);
 
+    float camLength = 1.0; //
+    float theta = 30; //viewing angle
+    float angle = tan((theta/2.0) * (M_PI / 180.0));
+    float aspect = (float(screenX)/screenY); //aspect ratio
+    int N = 300; //distance between eye and screen
+    float H = N * tan((theta/2.0) * (M_PI / 180.0));
+    float W = H * aspect;
+    float indWidth = 1/float(screenX);
+    float indHeight = 1/float(screenY);
+
+
+
     glBegin(GL_POINTS);
-//        Coordinate eye(-3, -3, -3, 1);
-//
+        Coordinate eye(0.0, 0.0, 0.0, 1);
+        Coordinate lookPoint(0.0, 2.0, 0.0, 1);
+        Coordinate lookVector = lookPoint - eye;
+        lookVector.normalise();
+        Coordinate up(0.0, 0.0, 1.0, 0);
+
+
+        Coordinate u = lookVector.cross(up);
+        u.normalise();
+        Coordinate v = u.cross(lookVector);
+        v.normalise();
+        u = u * screenX;
+        v = v * screenY;
+
+        Coordinate screenCenter = eye + (lookVector * camLength);
+
+        for (int r = 0; r < screenY*2; r++) {
+            for (int c = 0; c < screenX*2; c++) {
+                Coordinate screenWorld1 = screenCenter + (u * (c/float(screenX) - 1));
+                Coordinate screenWorldCoordinate = screenWorld1 + (v * (r/float(screenY) - 1));
+
+                Ray screenRays1(eye,screenWorldCoordinate);
+                if (sphere3.hit(screenRays1).isPoint()) {
+                    glColor3f(sphere3.getR(), sphere3.getG(), sphere3.getB());
+                glVertex2i(screenWorldCoordinate.getX(), screenWorldCoordinate.getZ());
+                }
+
+            }
+        }
+
 //        for (int r = 0; r < screenY; r++) {
 //            for (int c = 0; c < screenX; c++) {
+//                float xx = (2 * ((c + 0.5) * indWidth) - 1) * angle * aspect;
+//                float yy = (1 - 2 * ((r + 0.5) * indHeight)) * angle;
+//                Coordinate raydir(xx, yy, -1, 0);
+//                //raydir.normalize();
+//                Ray screenRays1(eye,raydir);
+//                if (sphere3.hit(screenRays1).isPoint()) {
+//                    glColor3f(sphere2.getR(), sphere2.getG(), sphere2.getB());
+//                    glVertex2i(raydir.getX()+W, raydir.getY()+H);
+//                }
+//
 //                float x=0,y=0,z=0;
 //                x = eye.getX() - N;
 //                y = eye.getY() + u[c];
@@ -225,13 +307,13 @@ int main(int argc, char *argv[]) {
 //                    glColor3f(sphere2.getR(), sphere2.getG(), sphere2.getB());
 //                    glVertex2i(screenVec.getX()+W, screenVec.getY()+H);
 //                }
-//
 //            }
 //        }
 
-//        for (int i = 0; i < 500; i++) {
-//                glVertex2i(i, i);
-//        }
+        for (int i = 0; i < 500; i++) {
+            glColor3f(0, 153, 255);
+            glVertex2i(i, i);
+        }
     glEnd();
     glFlush();
 
